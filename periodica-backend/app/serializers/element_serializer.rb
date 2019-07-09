c2636d72e7202da220a45a8e7eed7087b70455d9
mbarrins <maija.barrins@gmail.com>
2019-07-09 12:01:56 +0100

    added user_quiz_element_id when exists to element serializer

--- periodica-backend/app/serializers/element_serializer.rb
+++ periodica-backend/app/serializers/element_serializer.rb
@@ -3,6 +3,16 @@ class ElementSerializer < ActiveModel::Serializer
       :meltingPoint, :boilingPoint, :electronegativity, 
       :atomicWeight, :classification_id
 
+  def attributes(*args)
+    hash = super
+
+    if self.object.user_quiz_elements.find_by(user_id: @instance_options[:user_id].to_i)
+      hash[:user_quiz_element_id] = self.object.user_quiz_elements.find_by(user_id: @instance_options[:user_id].to_i).id
+    end
+
+    hash
+  end
+
   attribute :classification_name do
     self.object.classification.name
   end
