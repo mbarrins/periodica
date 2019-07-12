6df6327efa5577a43906dc14c4baafd4b5a4c95a
mbarrins <maija.barrins@gmail.com>
2019-07-12 09:24:14 +0100

    added no_of_ques column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_10_153649) do
+ActiveRecord::Schema.define(version: 2019_07_12_082206) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -94,6 +94,7 @@ ActiveRecord::Schema.define(version: 2019_07_10_153649) do
     t.string "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "no_of_ques", default: 10
   end
 
   add_foreign_key "elements", "classifications"
