b6e38b1b5614951ac022d4393839c9980a39642a
mbarrins <maija.barrins@gmail.com>
2019-07-12 10:00:00 +0100

    removed unnecessary routes for classifications

--- app/controllers/classifications_controller.rb
+++ app/controllers/classifications_controller.rb
@@ -1,26 +1,8 @@
 class ClassificationsController < ApplicationController
-  before_action :set_classification, only: [:show]
-
   # GET /classifications
   def index
     @classifications = Classification.all
 
     render json: @classifications
   end
-
-  # GET /classifications/1
-  def show
-    render json: @classification
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_classification
-      @classification = Classification.find(params[:id])
-    end
-
-    # Only allow a trusted parameter "white list" through.
-    def classification_params
-      params.require(:classification).permit(:name, :description)
-    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :elements, only: [:show, :index]
-  resources :classifications, only: [:show, :index]
+  resources :classifications, only: [:index]
   resources :quiz_questions
   resources :user_quiz_elements
   resources :quizzes, only: [:show, :index, :create, :update]
