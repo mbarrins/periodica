1b038910677467c4634ed18003d979fa2c924329
mbarrins <maija.barrins@gmail.com>
2019-07-12 10:05:34 +0100

    removed unnecessary routes for user quiz elements

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :elements, only: [:index]
   resources :classifications, only: [:index]
   resources :quiz_questions
-  resources :user_quiz_elements
+  resources :user_quiz_elements, only: [:create, :delete]
   resources :quizzes, only: [:show, :index, :create, :update]
   resources :users, only: [:show, :index, :create, :update]
 
