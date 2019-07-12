f41b13d9fa50af89049032b3838bb4a04617f773
mbarrins <maija.barrins@gmail.com>
2019-07-12 21:05:22 +0100

    added root route

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :create, :update]
   resources :subjects, only: [:index, :show]
 
+  root 'static#index'
   get '*other', to: 'static#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
